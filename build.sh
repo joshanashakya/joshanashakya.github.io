ae1badf51fb6460a69b1175204a6c020ee49f61c
joshanashakya <jshakya@gannett.com>
2025-04-10 15:22:55 +0545

    Update tar information

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-# Download the latest Quarto tarball release for Linux
 wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.22/quarto-1.7.22-linux-amd64.tar.gz
 
-# Extract the tarball into the current directory
-tar -xzf quarto-1.7.22-linux-amd64.tar.gz
+mkdir ~/opt
+tar -C ~/opt -xvzf quarto-1.7.22-linux-amd64.tar.gz
 
-# Add the extracted binary directory to the PATH for this build session
-export PATH="$PWD/quarto/bin:$PATH"
+mkdir ~/.local/bin
+ln -s ~/opt/quarto-1.7.22/bin/quarto ~/.local/bin/quarto
 
 # Verify the installation by checking Quarto version
 quarto --version
