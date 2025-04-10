96e7e70645059a060a4b8b02876f1ae8c55a6b87
joshanashakya <jshakya@gannett.com>
2025-04-10 14:41:54 +0545

    Remove root privilege while installation

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 # Download and install the latest Quarto deb package (for Linux)
 curl -LO https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.22/quarto-1.7.22-linux-amd64.deb
-sudo dpkg -i quarto-1.7.22-linux-amd64.deb
+dpkg -i quarto-1.7.22-linux-amd64.deb
 
 # (Optional) Check Quarto version to verify installation
 quarto --version
